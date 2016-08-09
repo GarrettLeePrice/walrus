fb23a06b8243dd577f8377ad68ee2efab7fd5e70
garrett price and josh casuccio <garrett@>
2016-08-09 09:50:34 -0700

    adds js styles and clickable elements

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#image-hidden").fadeToggle();
     $("#image-showing").fadeToggle();
-  });
 
+  });
+    $(".slide").click(function() {
+    $("#fight-slide").slideDown();
+  });
 });
